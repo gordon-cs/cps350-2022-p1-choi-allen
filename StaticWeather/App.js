6be3ae16ea97e388311cb9f14d50b794b1053ccb
Justin Choi <justin.choi@gordon.edu>
2022-02-01 22:14:35 -0500

    a4

--- StaticWeather/App.js
+++ StaticWeather/App.js
@@ -3,11 +3,12 @@
  */
 
  import React, { Component } from 'react';
+ import { StatusBar } from 'expo-status-bar';
  import {
    Text,
    View,
+   StyleSheet
  } from 'react-native';
- 
  import WeatherNow from './src/WeatherNow';
  
  export default class StaticWeatherApp extends Component {
@@ -49,9 +50,19 @@
        }
      };
      return (
-         <View>
-           <WeatherNow weatherData={weatherData} />
-         </View>
+      <View style={styles.container}>
+      <WeatherNow/>
+      <StatusBar style="auto" />
+    </View>
      )
    }
  }
+
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
